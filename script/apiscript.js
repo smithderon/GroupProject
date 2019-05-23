9b1ee63730a871ca1ca810e43fd2b9b9be16c492
Deron Smith <smithderon42@yahoo.com>
2019-05-23 10:44:37 -0500

    addendum

--- script/apiscript.js
+++ script/apiscript.js
@@ -1,44 +1,60 @@
+$(function(){
 //Video juegos
+("$.#videoGames").addListener("on", "click", function(){
+    $.get(`https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`)
+    .$.done(function (it) {
+        return it.json()
+    })
+    .$.done((returned) => {
+        console.log(returned)
+        ("$.#piece1").html("<h2>" + returned[0].question + "</h2>")
+    })
+    
+})
 
-$.get(`https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`)
-    .$.done(function (it) {
-        return it.json()
-    })
-    .$.done((returned) => {
-        console.log(returned)
-    })
 //General knowledge
-$.get(`https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`)
-    .$.done(function (it) {
+
+
+("$.#gnk").addListener("on","click", function(){
+    $.get(`https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`)
+    $.done(function (it) {
         return it.json()
     })
-    .$.done((returned) => {
+    $.done((returned) => {
         console.log(returned)
     })
+})
 
 //mixed
-$.get(`https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple`)
-    .$.done(function (it) {
+("$.#mixed").addListener("on","click",function(){
+
+    $.get(`https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple`)
+    $.done(function (it) {
         return it.json()
     })
-    .$.done((returned) => {
+    $.done((returned) => {
         console.log(returned)
     })
 
+})
 //History
-$.get(`https://opentdb.com/api.php?amount=10&category=23`)
-    .$.done(function (it) {
+(`$.#history`).addListener("on", "click", function(){
+    $.get(`https://opentdb.com/api.php?amount=10&category=23`)
+    $.done(function (it) {
         return it.json()
     })
-    .$.done((returned) => {
+    $.done((returned) => {
         console.log(returned)
     })
-
+})
 //Japanese Anime/Manga
-$.get(`https://opentdb.com/api.php?amount=10&category=31&difficulty=medium`)
-    .$.done(function (it) {
+("$.#jap").addListener("on","click", function(){
+    $.get(`https://opentdb.com/api.php?amount=10&category=31&difficulty=medium`)
+    $.done(function (it) {
         return it.json()
     })
-    .$.done((returned) => {
+    $.done((returned) => {
         console.log(returned)
     })
+})
+})
