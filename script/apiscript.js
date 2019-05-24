3e5b5ab8b77bfcfbcce972e68f6ed65d12977a33
Alfred Santos <alfie.g.santos@gmail.com>
2019-05-23 19:44:30 -0500

    update

--- script/apiscript.js
+++ script/apiscript.js
@@ -3,13 +3,15 @@
 
 //~~~~VIDEO GAME SECTION~~~~
 
-//for loop generates 9 questions from the API and appends them to hider cards
+
+
+//retrieves question from API and appends it to the modal dialog box
 let url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple"
 $.get(url).done((response) => {
-    for(i=1; i<10; i++){
-    $("#hider-"+i).text(response.results[i].question)
-    }
+    $("#question-modal").text(response.results[1].question)
 })
+
+
 
 //for loop that removes each hider card and text on click
 for(i=1; i<10; i++){
@@ -19,12 +21,29 @@ $("#hider-"+i).click(function () {
 })
 }
 
+//ALFIE'S OLD CODE
+// //for loop generates 9 questions from the API and appends them to hider cards
+// let url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple"
+// $.get(url).done((response) => {
+//     for(i=1; i<10; i++){
+//     $("#hider-"+i).text(response.results[i].question)
+//     }
+// })
+
+
+
+
+
+
+
+
+
 
 //**** TO DO ****
 //1. make the other question categories
 //2. add multiple choice options to hider cards
 //3. make cards disappear when user answers question correcty, rather than clicking
-
+//4. make the modal appear with a keystroke rather than by clicking a button
 
 
 
