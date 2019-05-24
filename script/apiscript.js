1eea81d3bd0e2c6c2685a07fcb9d5b4b45bd0dc8
Deron Smith <smithderon42@yahoo.com>
2019-05-24 11:58:37 -0500

    WORKS

--- script/apiscript.js
+++ script/apiscript.js
@@ -1,26 +1,45 @@
 $(function(){
-
+    let count = 0
 //Video juegos
-$("#videoGames").click(function(){
-    $.get(`https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`)
-    .$.done(function (it) {
-        return it.json()
-    })
-    .$.done((returned) => {
-        console.log(returned)
-        $("#piece1").html("<h2>" + returned[0].question + "</h2>")
-    })
+    $("#btnModal").click(function(){
+
     
+    $.get(`https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`)
+    .done(function (it){
+
+        console.log(it.results)
+        $("#question").html(it.results[count].question)
+        $("#Answer1").html(it.results[count].incorrect_answers[1])
+        $("#Answer2").html(it.results[count].incorrect_answers[2])
+        $("#Answer3").html(it.results[count].incorrect_answers[0])
+        $("#Answer4").html(it.results[count].correct_answer)
+
+    })
 })
-
-
+let wrong = () => {
+    for(let i = 0; i <= count; i ++){
+        $("#hider-" + i).html("Wow, looks like you failed, huh?")
+    }
+    count = 0 
+}
+$("#Answer4").click(function(){
+    count += 1
+    if (count != 5){
+    $("#hider-" + count).html("Ohoho, Got it right, did ya? Click me, now!")
+    }
+})
+for (var g = 1; g <= 3; g ++){
+    $("#Answer" + g).click(function(){
+        wrong()
+    })
+}
 //General knowledge
 
 
 $("#gnk").click( function(){
     $.get(`https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`)
-    $.done(function (it) {
-        return it.json()
+    .done(function (it) {
+        
     })
     $.done((returned) => {
         console.log(returned)
